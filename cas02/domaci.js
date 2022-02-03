c1e8142a01dac8d430f205fa39c08a8a9e3133c3
Miloš Vojnović <mvojnovic@smart.edu.rs>
2022-02-03 12:44:57 +0100

    first commit

---
+++ cas02/domaci.js
@@ -0,0 +1,24 @@
+//Ispisuje prosledjeni niz brojeva jedan ispod drugog.
+var a = [ 3,4,5,6,7,8];
+for( i of a )
+    console.log(i)
+//Od prosledjenjog niza brojeva ispisuje samo parne.
+for(i of a){
+    if(a%2==0){
+        console.log(i)
+    }
+}
+//Racuna sumu prosledjenog niza.
+var zbir
+for(i of a){
+    zbir+=a[i]  
+}
+console.log(zbir)
+//Ispisuje koliko ima brojeva vecih od prvog elementa niza.
+var brojac=0
+for(i of a){
+    if(a[0]>a[i+1])
+        brojac++
+    
+}
+console.log(brojac)
