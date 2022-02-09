0e8f1e8930df93cce0eb1e18580fe5bea519b8a7
Miloš Vojnović <mvojnovic@smart.edu.rs>
2022-02-09 08:17:33 +0100

    first commit

---
+++ ponavljanje.js
@@ -0,0 +1,26 @@
+//1
+var niz=[4,5,6,,'g',5.6,{},-6]
+3 == NaN
+typeof(4)
+typeof('5') 
+typeof({})
+if(typeof(4)== 'number')
+console.log('jeste broj')
+for(el in niz )
+if  (typeof(el)== 'number')
+console.log(el)
+for(el in niz )
+if  (typeof(el)== 'number')
+console.log(el)
+var br=0
+for(el in niz )
+if  (typeof(el)== 'number')
+br++;
+//2
+niz=[4,5,6,7,,5,6,7,2,4,32,1,4,7,0]
+niz=[4,5,6,7,0,5,6,7,2,4,32,1,4,7,0]
+for(i=0;i<niz.length;i++)
+
+    if(niz[i-1]<niz[i])
+
+       console.log(niz[i]) 
