e326f77b44654a01bd6266f53e026b3a9dc66d6a
Miloš Vojnović <mvojnovic@smart.edu.rs>
2022-03-17 18:02:29 +0100

    rad sa casa

--- Cas06/vezba1.js
+++ Cas06/vezba1.js
@@ -1,4 +1,25 @@
-MediaKeySessions
-dispatchEventss
-
-SVGFEDropShadowElements
+    const array=[4,5,0,3,6,7]
+    function moj_reduser(prosli,sadasnji){
+        return prosli+ sadasnji;
+    }
+array.reduce(moj_reduser)
+25
+array.reduce(moj_reduser,-5)
+20
+const getMax= (a,b)=> Math.max(a,b)
+[4,5,6,7].reduce(getMax)
+7
+[4,5,6,7].reduce(getMax)
+7
+const funkcija = function(x){return x+4;}
+[5,6,7].reduce(getMax)
+13
+const funkcija = function(x){console.lod (x);return x+4;}
+[5,6,7].reduce(funkcija)
+15
+9
+13
+const funkcija = function(x,y){console.lod ("X="+x+"Y=");return x-y;}
+[5,6,7].reduce(funkcija)
+x=5 y=6
+x=-1 y=7
